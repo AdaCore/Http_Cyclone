df932024ae8e2b792f282da3a4ac63858ceb344b
Kyriakos Georgiou <kg8280@bristol.ac.uk>
2020-11-06 16:04:20 +0000

    adding license headers to Ada files

--- src/ada/tcp_timer_interface.ads
+++ src/ada/tcp_timer_interface.ads
@@ -1,11 +1,27 @@
-----------------------------------------------------
---                                                --
--- This file only contains some proofs to be used --
--- in tcp.adb. The proof models transitions       --
--- between states that can happen when the timer  --
--- ellapsed.                                      --
---                                                --
-----------------------------------------------------
+------------------------------------------------------------------------------
+--                              HTTP_Cyclone                                --
+--                                                                          --
+--                        Copyright (C) 2020, AdaCore                       --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- LICENSE  If not, go to http://www.gnu.org/licenses for a complete copy   --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+-- This file only contains some proofs to be used                           --
+-- in tcp.adb. The proof models transitions                                 --
+-- between states that can happen when the timer                            --
+-- ellapsed.                                                                --
+------------------------------------------------------------------------------
+
 
 with Common_Type;  use Common_Type;
 with Socket_Types; use Socket_Types;
