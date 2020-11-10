47ca580b3b4ded998e30e44d82107ac6e852dcf1
Kyriakos Georgiou <kg8280@bristol.ac.uk>
2020-11-10 20:55:05 +0000

    fixing the language usage

--- configure.sh
+++ configure.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 cat << EOF
-Chose your material in the list below to configure the Http_Cyclone demo.
-[a] stm32f407
+Chose the development board to be used for compiling the Http_Cyclone demo.
+[a] stm32f407_discovery
 [b] stm32f769i_discovery
 EOF
 
-read -p 'Select your material: ' material
+read -p 'Please, select an option: ' material
 
 if [ $material = 'a' -o $material = 'A' ]
 then
-    echo 'Done. Run "make" to compile the demo'
+    echo 'Done. Run "make" to compile the demo.'
     cp Makefile.stm32f4xx Makefile
 elif [ $material = 'b' -o $material = 'B' ]
 then
-    echo 'Done. Run "make" to compile the demo'
+    echo 'Done. Run "make" to compile the demo.'
     cp Makefile.stm32f7xx Makefile
 else
-    echo 'Your material is not supported'
+    echo 'Unsupported option.'
 fi
